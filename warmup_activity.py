1e012ffbfa7cecbfab0f0e3df9460332cb20984e
mecha-212 <boagmatt@gmail.com>
2024-05-26 17:51:45 +1000

    Updated Warm Up activity

--- warmup_activity.py
+++ warmup_activity.py
@@ -6,9 +6,32 @@
 name = "Matthew Boag"
 favNumber = 4
 
-# Print name
+#PART 1
+# Print my name
 print(name)
 
 # Count up to the square of the favorite number
 for i in range(1, favNumber**2 + 1):
     print(i)
+
+#PART 3
+# Define the Engineers class
+class Engineers:
+    skill = "problem solver"
+
+    def __init__(self, name, type, experience):
+        self.name = name
+        self.type = type
+        self.experience = experience
+
+# Create two different engineers (objects)
+engineer1 = Engineers("Tobias Low", "Mechatronic", 12)
+engineer2 = Engineers("Jason Brown", "Electronic and Computer System", 13)
+
+# Print out their attributes
+print(f"Engineer 1: Name: {engineer1.name}, Type: {engineer1.type}, Years of Experience: {engineer1.experience}, Skill: {Engineers.skill}")
+print(f"Engineer 2: Name: {engineer2.name}, Type: {engineer2.type}, Years of Experience: {engineer2.experience}, Skill: {Engineers.skill}")
+
+#PART 3A
+# My name
+print(name[::-1])
